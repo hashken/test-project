c15e56d51e5f60cc2616b2401fd3af06f6a6acf7
Karthik K <hashken.distro@gmail.com>
2018-01-17 12:45:32 +0530

    Initial Commit

---
+++ test.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/17/2018 12:44:13
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+int main (int argc, char** argv) {
+    std::cout << "Hello";
+
+    return 0;
+}
